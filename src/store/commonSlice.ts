db384447d724de343ef1ad234a235ed950091e2f
法外狂徒李四 <dimensional_logic@qq.com>
2022-10-06 16:56:40 +0800

    【i】【CmcOrg】

--- src/store/commonSlice.ts
+++ src/store/commonSlice.ts
@@ -1,12 +1,9 @@
 import {createSlice} from '@reduxjs/toolkit'
 
 interface ICommonSlice {
-    rsaPublicKey: string // 非对称：公钥
 }
 
-const initialState: ICommonSlice = {
-    rsaPublicKey: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDadmaCaffN63JC5QsMK/+le5voCB4DzOsV9xOBZgGJyqnizh9/UcFkIoRae5rebdWUtnPO4CTgdJbuSvu/TtIIPj9De5/wiJilFAWd1Ve7qGaxxTxqWwFNp7p/FLr0YpMeBjOylds9GyA1cnjIqruNdYv+qRZnseE0Sq2WEZus9QIDAQAB',
-}
+const initialState: ICommonSlice = {}
 
 export const commonSlice = createSlice({
     name: 'commonSlice',
