26b1bac231bc8fd896c1c481357f8f5ba4a8c92d
caoguiming <1677664380@qq.com>
2022-11-08 09:42:53 +0800

    【i】【CmcOrg】

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import {resolve} from 'path'
+import {version} from "antd";
 
 // https://vitejs.dev/config/
 export default ({mode}) =>
     defineConfig({
+        define: {
+            'process.env.ANTD_VERSION': `"${version}"`,
+        },
         plugins: [react()],
         resolve: {
             alias: [
